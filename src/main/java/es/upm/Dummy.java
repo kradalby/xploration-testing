301ec1a873ff62fe5fefc48f23893baf11c36bc0
Kristoffer Dalby <kradalby@kradalby.no>
2016-04-27 00:28:40 +0200

    adds dummy java file

---
+++ src/main/java/es/upm/Dummy.java
@@ -0,0 +1,10 @@
+package es.upm;
+
+/**
+ * Created by kradalby on 27/04/16.
+ */
+public class Dummy {
+    public static void main(String[] args) {
+        System.out.println("dumdumdum");
+    }
+}
